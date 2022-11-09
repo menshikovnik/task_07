2dff62af2dc54b1504c9ac9dacb05ea9f831f591
nikmenshikov <kolya.menshikov.03@list.ru>
2022-11-09 23:22:27 +0300

    beta

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        getFactorial();
+//        Scanner scanner = new Scanner(System.in);
+//        int[] myArray;
+//        myArray = new int[10];
+//        for (int i = 0; i < myArray.length; i++) {
+//            myArray[i] = scanner.nextInt();
+//        }
     }
+    public static void getFactorial() {
+        long result = 1;
+        long sum = 0;
+        int [] myArray = new int [] {120, 6, 12, 15, 479001600};
+        for (int i = 1;i <= 63; i++) {
+            result = result * i;
+            for (int j = 0;j < myArray.length;j++){
+               if (myArray[j] == result){
+                   sum += myArray[j];
+               }
+            }
+        }
+        System.out.println(sum);
+    }
+
+//
+//    public static boolean check (int [] arr){
+//        if ()
+//    }
 }
---
+++ src/Penis.java
@@ -0,0 +1,9 @@
+public class Penis {
+    public static int getFactorial(int f) {
+        int result = 1;
+        for (int i = 1; i <= f; i++) {
+            result = result * i;
+        }
+        return result;
+    }
+}
