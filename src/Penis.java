9415648a9f89f8e2fe3ac04d3a3866b92d27ce99
nikmenshikov <kolya.menshikov.03@list.ru>
2022-11-09 23:29:13 +0300

    beta.02

--- src/Penis.java
+++
@@ -1,9 +0,0 @@
-public class Penis {
-    public static int getFactorial(int f) {
-        int result = 1;
-        for (int i = 1; i <= f; i++) {
-            result = result * i;
-        }
-        return result;
-    }
-}
