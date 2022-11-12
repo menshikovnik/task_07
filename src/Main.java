b8d86a1438ca0bc10e47c555b4738c60c8afb023
nikmenshikov <kolya.menshikov.03@list.ru>
2022-11-13 02:15:46 +0300

    pre-release

--- src/Main.java
+++ src/Main.java
@@ -2,31 +2,43 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        getFactorial();
-//        Scanner scanner = new Scanner(System.in);
-//        int[] myArray;
-//        myArray = new int[10];
-//        for (int i = 0; i < myArray.length; i++) {
-//            myArray[i] = scanner.nextInt();
-//        }
-    }
-    public static void getFactorial() {
-        long result = 1;
-        long sum = 0;
-        int [] myArray = new int [] {120, 6, 12, 15, 479001600};
-        for (int i = 1;i <= 63; i++) {
-            result = result * i;
-            for (int j = 0;j < myArray.length;j++){
-               if (myArray[j] == result){
-                   sum += myArray[j];
-               }
-            }
-        }
-        System.out.println(sum);
+        System.out.println("Сумма равна: " + inputValue());
     }
 
-//
-//    public static boolean check (int [] arr){
-//        if ()
-//    }
+    public static long Solution(long[] arr) {
+        long sum = 0;
+        for (int j = 0; j < arr.length; j++) {
+            if (isFactorial(arr[j])) {
+                sum += arr[j];
+            }
+        }
+        return sum;
+    }
+
+    public static long inputValue() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введіть розмір масиву: ");
+        final int SIZE = sc.nextInt();
+        long[] myArray = new long[SIZE];
+        for (int i = 0; i < myArray.length; i++) {
+            System.out.print("Введите элемент массива номер " + i + ": ");
+            myArray[i] = sc.nextInt();
+        }
+        return Solution(myArray);
+    }
+
+    static boolean isFactorial(long n) {
+        for (int i = 1; ; i++) {
+            if (n % i == 0) {
+                n /= i;
+            } else {
+                break;
+            }
+        }
+        if (n == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
