2bf5d81b9d10eeec9b08b622fd7cb740fef3f50e
nikmenshikov <kolya.menshikov.03@list.ru>
2022-11-16 14:30:30 +0300

    release with tests

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        test1();
-        test2();
-        test3();
-        test4();
-        test5();
-        test6();
+        System.out.println("Сумма равна: " + test1());
+        System.out.println("Сумма равна: " + test2());
+        System.out.println("Сумма равна: " + test3());
+        System.out.println("Сумма равна: " + test4());
+        System.out.println("Сумма равна: " + test5());
         System.out.println("Сумма равна: " + inputValue());
     }
 
@@ -24,14 +23,14 @@ public class Main {
 
     public static long inputValue() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Введите размер массива: ");
-        final int SIZE = sc.nextInt();
-        long[] myArray = new long[SIZE];
-        for (int i = 0; i < myArray.length; i++) {
-            System.out.print("Введите элемент массива номер " + i + ": ");
-            myArray[i] = sc.nextInt();
+        System.out.println("Введите элементы массива: ");
+        String line = sc.nextLine();
+        String[] lineArray = line.split(" ");
+        long[] numbers = new long[lineArray.length];
+        for (int i = 0; i < lineArray.length; i++) {
+            numbers[i] = Integer.parseInt(lineArray[i]);
         }
-        return Solution(myArray);
+        return Solution(numbers);
     }
 
     static boolean isFactorial(long n) {
@@ -42,76 +41,36 @@ public class Main {
                 break;
             }
         }
-        if (n == 1) {
-            return true;
-        } else {
-            return false;
-        }
+        return n == 1;
     }
-    public static void test1() {
-        long sum = 0;
-        int [] a = new int[]{2, 6, 24, 120};
+
+    public static long test1() {
+        long[] a = new long[]{2, 6, 24, 120};
         System.out.println(Arrays.toString(a));
-        for (int j = 0; j < a.length; j++) {
-            if (isFactorial(a[j])) {
-                sum += a[j];
-            }
-        }
-        System.out.println(sum);
+        return Solution(a);
     }
-    public static void test2() {
-        int [] a = new int[]{3, 7, 25, 121};
+
+    public static long test2() {
+        long[] a = new long[]{1, 1, 6, 6};
         System.out.println(Arrays.toString(a));
-        long sum = 0;
-        for (int j = 0; j < a.length; j++) {
-            if (isFactorial(a[j])) {
-                sum += a[j];
-            }
-        }
-        System.out.println(sum);
+        return Solution(a);
     }
-    public static void test3() {
-        int [] a = new int[]{3, 6, 24, 121};
+
+    public static long test3() {
+        long[] a = new long[]{25, 121, 7, 10};
         System.out.println(Arrays.toString(a));
-        long sum = 0;
-        for (int j = 0; j < a.length; j++) {
-            if (isFactorial(a[j])) {
-                sum += a[j];
-            }
-        }
-        System.out.println(sum);
+        return Solution(a);
     }
-    public static void test4() {
-        int [] a = new int[]{1, 1};
+
+    public static long test4() {
+        long[] a = new long[]{1, 1, 1, 1};
         System.out.println(Arrays.toString(a));
-        long sum = 0;
-        for (int j = 0; j < a.length; j++) {
-            if (isFactorial(a[j])) {
-                sum += a[j];
-            }
-        }
-        System.out.println(sum);
+        return Solution(a);
     }
-    public static void test5() {
-        int [] a = new int[]{1, 1, 6, 6};
+
+    public static long test5() {
+        long[] a = new long[]{0, 0, 0, 0};
         System.out.println(Arrays.toString(a));
-        long sum = 0;
-        for (int j = 0; j < a.length; j++) {
-            if (isFactorial(a[j])) {
-                sum += a[j];
-            }
-        }
-        System.out.println(sum);
-    }
-    public static void test6() {
-        int [] a = new int[]{1, 1, 7, 5040};
-        System.out.println(Arrays.toString(a));
-        long sum = 0;
-        for (int j = 0; j < a.length; j++) {
-            if (isFactorial(a[j])) {
-                sum += a[j];
-            }
-        }
-        System.out.println(sum);
+        return 0;
     }
 }
